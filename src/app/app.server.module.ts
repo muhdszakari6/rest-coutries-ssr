40edd44f800416ac8d3f4244a8388183a603ac0e
muhdszakari6 <salimzakari6@gmail.com>
2026-07-24 17:18:09 +0100

    Clean ups

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,19 +3,11 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { EnvironmentService } from './services/environment.service';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-  ],
-  providers: [
-    {
-      provide: EnvironmentService,
-      useValue: { ...environment, api_key: process.env['API_KEY'] || '' }
-    }
   ],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_url: 'https://api.restcountries.com/countries/v5',
-  api_key: ''
+  api_key: process.env['API_KEY'] || ''
 };
