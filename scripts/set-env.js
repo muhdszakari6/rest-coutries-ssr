f7d73eae37f3e5b60a118d8685ef8df7c5fa3ff0
muhdszakari6 <salimzakari6@gmail.com>
2026-07-27 16:02:06 +0100

    Refactoring WIP

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -2,14 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const apiKey = process.env['API_KEY'] || '';
+const envDir = path.join(__dirname, '..', 'src', 'environments');
 
-const envContent = `export const environment = {
-  production: true,
+const files = [
+  { file: 'environment.ts', production: false },
+  { file: 'environment.prod.ts', production: true },
+];
+
+files.forEach(({ file, production }) => {
+  const content = `export const environment = {
+  production: ${production},
   base_url: 'https://api.restcountries.com/countries/v5',
   api_key: '${apiKey}'
 };
 `;
-
-const targetPath = path.join(__dirname, '..', 'src', 'environments', 'environment.prod.ts');
-fs.writeFileSync(targetPath, envContent);
-console.log(`Written API_KEY to ${targetPath}`);
+  const targetPath = path.join(envDir, file);
+  fs.writeFileSync(targetPath, content);
+  console.log(`Written API_KEY to ${targetPath}`);
+});
